9c515e87b1a3a56d1e804eda9c6b1d0e385c0a86
byour13 <78316297+byour13@users.noreply.github.com>
2021-06-28 13:55:27 +0800

    Add files via upload

---
+++ shadowrocketTikTok.js
@@ -0,0 +1,15 @@
+/*
+方法解析：
+点击小火箭的配置，选择编辑纯文本，将下面内容分别复制粘贴至文本中对应[URL Rewrite]&[mitm]；
+接下来点击编辑配置，进入HTTPS解密打开开关，如需安装证书就按照流程进行
+*/
+
+[URL Rewrite]
+(?<=_region=)CN(?=&) US 307
+(?<=&mcc_mnc=)4 2 307
+^(https?:\/\/tnc[\w-]+\.\w+\.com\/.+)(\?)(.+) $1$3 302
+^(https?:\/\/dm[\w-]+\.\w+\.com\/.+)(\?)(.+) $1$3 302
+(^https?:\/\/*\.\w{4}okv.com\/.+&.+)(\d{2}\.3\.\d)(.+) $118.0$3 302
+
+[mitm]
+hostname= *.tiktokv.com,*.byteoversea.com,*.tik-tokapi.com
